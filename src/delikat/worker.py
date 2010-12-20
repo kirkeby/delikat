6da24f7818b2be690862f3743023027e4ea25474
Sune Kirkeby <mig@ibofobi.dk>
2010-12-20 13:18:13 +0100

    Remove debug print statements.

--- src/delikat/worker.py
+++ src/delikat/worker.py
@@ -6,10 +6,8 @@ def main():
     with Store() as store:
         while True:
             new_link = store.pop_queue('new-link')
-            print 'new-link', `new_link`
             try:
                 store.do_save_link(new_link)
-                print('Created link for %(user)s: %(url)s' % new_link)
             except Exception:
                 store.push_queue('failed', {
                     'queue': 'new-link',
